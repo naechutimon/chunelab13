98806587d6207740eb28f0a123ae0ae280ce28db
naechutimon <chutimonnaenae@gmail.com>
2024-12-27 17:38:52 +0700

    chunelab13/2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,34 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool pic[][M]){
+    cout<<"------------------------------------------------------------------------"<<"\n";
+    
+    for(int i=0;i<30;i++){
+        cout << "|";
+        for(int j=0;j<70;j++){
+            if(pic[i][j]==0){
+                cout<<" ";
+            }else{
+                cout<<"*";
+            }
+        }
+        cout << "|" <<"\n";
+    }
+
+    cout<<"------------------------------------------------------------------------"<<"\n";
+}
+
+void updateImage(bool pic[][M],int s,int x,int y){
+    for(int i =0;i<30;i++){
+        for(int j=0;j<70;j++){
+            if ((sqrt(pow(i-x,2)+pow(j-y,2))) <= (s-1)){
+                pic[i][j]=1;
+            }
+        }
+    }
+}
+
+
+
